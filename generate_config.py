fe67037f967ae4ee2c80ea93183dea4720734fe7
Eduardo Santana <ezambomsantana@gmail.com>
2019-08-04 23:24:49 -0300

    scenarios-2-3

--- generate_config.py
+++ generate_config.py
@@ -1,15 +1,23 @@
-for x in range(0,21):
+import os
 
-  file1 = open("scenario-4-5/config" + str(x) + ".xml","w") 
-  file1.write("<scsimulator_config>\n")
-  file1.write(" <config \n")
-  file1.write("     trip_file=\"../scenario5/trips" + str(x) + ".xml\"\n")
-  file1.write("      map_file=\"../scenario5/network.xml\"\n")
-  file1.write("      output_file=\"../output/events.xml\"\n")
-  file1.write("      traffic_signals_file=\"../scenario5/signals.xml\"\n")
-  file1.write("      digital_rails_file=\"../scenario5/empty-digital-rails.xml\"\n")
-  file1.write("      simulation_time=\"86400\"/>\n")
-  file1.write(" </scsimulator_config>")
+scenario_name = "scenario3"
+os.mkdir(scenario_name)
 
-  file1.close()
+for x in range (0, 11):
+  folder = scenario_name + "/" + str(x)
+  os.mkdir(folder)
+  for y in range(0,21):
+
+    file1 = open(folder + "/config" + str(y) + ".xml","w") 
+    file1.write("<scsimulator_config>\n")
+    file1.write(" <config \n")
+    file1.write("      trip_file=\"../" + folder + "/trips" + str(y) + ".xml\"\n")
+    file1.write("      map_file=\"../" + scenario_name + "/network.xml\"\n")
+    file1.write("      output_file=\"../output/events.xml\"\n")
+    file1.write("      traffic_signals_file=\"../" + scenario_name + "/signals.xml\"\n")
+    file1.write("      digital_rails_file=\"../" + scenario_name + "/empty-digital-rails.xml\"\n")
+    file1.write("      simulation_time=\"86400\"/>\n")
+    file1.write(" </scsimulator_config>")
+
+    file1.close()
 
